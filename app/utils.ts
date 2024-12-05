0e142267e948e7d68c070f3e52d75c0b6e01fe12
seeu <showqonline@gmail.com>
2024-12-05 10:18:45 +0800

    增加支持vision的模型名

--- app/utils.ts
+++ app/utils.ts
@@ -279,6 +279,8 @@ export function isVisionModel(model: string) {
     "gemini-1.5-flash",
     "gpt-4o",
     "gpt-4o-mini",
+    "gpt-4-plus",
+    "gpt-4-vision-preview",
   ];
   const isGpt4Turbo =
     model.includes("gpt-4-turbo") && !model.includes("preview");
